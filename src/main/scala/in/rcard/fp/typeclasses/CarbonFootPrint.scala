17d84f73e73dba63ad915722b33075d488bf3031
rcardin <riccardo.cardin@gmail.com>
2020-09-10 22:17:20 +0200

    Implemented Pollutant[T] typeclass

--- src/main/scala/in/rcard/fp/typeclasses/CarbonFootPrint.scala
+++ src/main/scala/in/rcard/fp/typeclasses/CarbonFootPrint.scala
@@ -1,5 +1,7 @@
 package in.rcard.fp.typeclasses
 
+import in.rcard.fp.typeclasses.CarbonFootPrint.MethodOverloading.{carbonFootPrint, frisona, me, superCar}
+
 object CarbonFootPrint {
 
   case class Cow(weight: Double)
@@ -23,5 +25,28 @@ object CarbonFootPrint {
     println(s"The carbon footprint of a frisona cow is ${carbonFootPrint(frisona)}")
     println(s"The carbon footprint of a super car is ${carbonFootPrint(superCar)}")
     println(s"My carbon footprint is ${carbonFootPrint(me)}")
+  }
+
+  trait Pollutant[T] {
+    def pollute(p: T): Double
+  }
+
+  object Implicits {
+    implicit val cowPollute: Pollutant[Cow] = (cow: Cow) => cow.weight * 8
+    implicit val carPollute: Pollutant[Car] = (car: Car) => car.hp * 3.4D
+    implicit val humanPollute: Pollutant[Human] = (human: Human) => human.age * 15.5D
+  }
+
+  object AdHocPolymorphism extends Data {
+    import Implicits._
+    def carbonFootPrint[T](obj: T)(implicit pollutant: Pollutant[T]): Double =
+      pollutant.pollute(obj)
+
+    def carbonFootPrintAltSyntax[T: Pollutant](obj: T): Double =
+      implicitly[Pollutant[T]].pollute(obj)
+
+    println(s"The carbon footprint of a frisona cow is ${carbonFootPrintAltSyntax(frisona)}")
+    println(s"The carbon footprint of a super car is ${carbonFootPrintAltSyntax(superCar)}")
+    println(s"My carbon footprint is ${carbonFootPrintAltSyntax(me)}")
   }
 }
