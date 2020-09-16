60c7b8c56f52358589e60b9f63c64325ff94824a
rcardin <riccardo.cardin@gmail.com>
2020-09-16 22:21:34 +0200

    Added examples on semigroups

--- build.sbt
+++ build.sbt
@@ -3,3 +3,7 @@ name := "road-to-fp-in-scala"
 version := "0.1"
 
 scalaVersion := "2.13.3"
+
+scalacOptions += "-Ypartial-unification"
+
+libraryDependencies += "org.typelevel" %% "cats-core" % "2.0.0"
--- src/main/scala/in/rcard/fp/semigroups/Pollution.scala
+++ src/main/scala/in/rcard/fp/semigroups/Pollution.scala
@@ -1,5 +1,7 @@
 package in.rcard.fp.semigroups
 
+import in.rcard.fp.semigroups.Pollution.ImplicitsGenericAddExample.{add, monthlyPollutions, pollutions}
+
 object Pollution {
 
   case class Pollution(dailyCO2Grams: Double)
@@ -31,4 +33,56 @@ object Pollution {
     println(s"Add two pollutions ${add(cowPollution, superCarPollution)}")
     println(s"Add two pollutions maps ${addPollutionMap(pollutions, monthlyPollutions)}")
   }
+
+  object GenericAddExample extends Data {
+
+    trait Addable[T] {
+      def add(a: T, b: T): T
+    }
+
+    def addWithAddable[K, V](pollutions: Map[K, V], toAdd: Map[K, V])(addable: Addable[V]): Map[K, V] = {
+      toAdd.foldLeft(pollutions) {
+        case (acc, (name, pollution)) =>
+          acc + (name -> acc.get(name).map(addable.add(_, pollution)).getOrElse(pollution))
+      }
+    }
+
+    import SimpleAddExample.add
+
+    println(s"Add two pollutions maps ${
+      addWithAddable(pollutions, monthlyPollutions) { (p1: Pollution, p2: Pollution) => add(p1, p2) }
+    }")
+  }
+
+  object ImplicitsGenericAddExample extends Data {
+
+    import GenericAddExample.Addable
+
+    implicit val pollutionAdd: Addable[Pollution] = SimpleAddExample.add
+
+    implicit def mapsAdd[K, V: Addable]: Addable[Map[K, V]] =
+      (a: Map[K, V], b: Map[K, V]) => a.foldLeft(b) {
+        case (acc, (key, value)) =>
+          acc + (key -> acc.get(key).map(implicitly[Addable[V]].add(_, value)).getOrElse(value))
+      }
+
+    def add[A: Addable](a1: A, a2: A): A = implicitly[Addable[A]].add(a1, a2)
+
+    println(s"Add two pollutions maps ${add(pollutions, monthlyPollutions)}")
+  }
+
+  object CatsGenericAddBalanceExample extends Data {
+
+    import cats.Semigroup
+
+    implicit val pollutionSemigroup: Semigroup[Pollution] =
+      (x: Pollution, y: Pollution) => Pollution(x.dailyCO2Grams + y.dailyCO2Grams)
+
+    def add[A: Semigroup](a1: A, a2: A): A = implicitly[Semigroup[A]].combine(a1, a2)
+
+    import cats.instances.map._
+
+    println(s"Add two pollutions maps ${add(pollutions, monthlyPollutions)}")
+  }
+
 }
